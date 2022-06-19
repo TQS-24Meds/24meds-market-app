448e83c268158e73f4788c3868a441f3a4da1276
artur-romao <arturomao@gmail.com>
2022-06-19 06:24:36 +0100

    add exceptions

---
+++ market/src/main/java/com/meds/market/exception/ExistentUserException.java
@@ -0,0 +1,9 @@
+package com.meds.market.exception;
+
+public class ExistentUserException extends RuntimeException {
+
+    public ExistentUserException(String message) {
+        super(message);
+    }
+
+}
