84b49d288d11e85939e2e53e4bdca56302b58d00
PauloGSP <paulogspereira@ua.pt>
2022-06-19 13:10:27 +0100

    preciso de checkar os html

--- market/src/main/java/com/meds/market/views/indexController.java
+++ market/src/main/java/com/meds/market/views/indexController.java
@@ -1,5 +1,39 @@
 package com.meds.market.views;
 
+import javax.servlet.http.HttpSession;
+
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.meds.market.model.Client;
+import com.meds.market.services.ClientService;
+
+
+@Controller
 public class indexController {
+
+    @Autowired
+    ObjectFactory<HttpSession> httpSessionFactory;
+
+    @Autowired 
+    ClientService clientsv;
+
+    @GetMapping("/index")
+    public ModelAndView index(Model model) throws NumberFormatException, ResourceNotFoundException {
+      HttpSession session = httpSessionFactory.getObject();
+      String clientmail= (String.valueOf(session.getAttribute("email_client"))); //not sure qual é o nome do ciusi
+      Client client = clientsv.getClientByEmail(clientmail);
+  
+
+      ModelAndView modelAndView = new ModelAndView();
+      modelAndView.setViewName("index");
+      return modelAndView;
+    }
     
 }
